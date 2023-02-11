01d8fd625737d4ab6e8ed80e64f5534e2e94fca2
vahidbagvand <103110394+vahidbagvand@users.noreply.github.com>
2023-02-11 14:36:59 +0330

    Update auto-type-text-using-python.py

--- auto-type-text-using-python.py
+++ auto-type-text-using-python.py
@@ -1,7 +1,7 @@
 import pyautogui
 import time
 time.sleep(2)
-pyautogui.typewrite("root")
+pyautogui.typewrite("user455")
 pyautogui.press("enter")
 time.sleep(1)
 pyautogui.typewrite("klxDe3fsdnRw23df")
